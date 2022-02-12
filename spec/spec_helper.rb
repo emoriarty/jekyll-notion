bf64279a48a724a07f473ebcc07a7babc48d2b37
Enrique Arias <enrique.arias.cervero@gmail.com>
2022-02-12 12:54:37 +0100

    (patch) Check if document is present (#28)
    * (chore) Check if document is present
    
    * rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,18 +11,16 @@ RSpec.configure do |config|
   config.filter_run :focus
 
   SOURCE_DIR = File.expand_path("fixtures/my_site", __dir__)
-  DEST_DIR   = File.expand_path("dest", __dir__)
+  SOURCE_DIR_2 = File.expand_path("fixtures/my_site_2", __dir__)
+  DEST_DIR = File.expand_path("dest", __dir__)
   NOTION_RESULTS = YAML.load_file(File.expand_path("fixtures/notion/results.yml", __dir__))
   NOTION_RESULTS_2 = YAML.load_file(File.expand_path("fixtures/notion/results_2.yml", __dir__))
+  NOTION_RESULTS_3 = YAML.load_file(File.expand_path("fixtures/notion/results_3.yml", __dir__))
   MD_FILES = Dir[File.expand_path("fixtures/md_files/*.md",
                                   __dir__)].each_with_object({}) do |file, memo|
     value = File.read(file)
     key = File.basename(file, ".md")
     memo[key] = value
-  end
-
-  def source_dir(*files)
-    File.join(SOURCE_DIR, *files)
   end
 
   def dest_dir(*files)
