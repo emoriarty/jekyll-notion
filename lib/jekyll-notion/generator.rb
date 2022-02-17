808b3ff9c6ad8da0faa8459a359fb426fd5b83a7
Enrique Arias <enrique.arias.cervero@gmail.com>
2022-02-17 06:41:40 +0100

    (chore) Refactor Generator class (#32)
    * (chore) Refactor Generator class
    
    * rubocop

--- lib/jekyll-notion/generator.rb
+++ lib/jekyll-notion/generator.rb
@@ -20,16 +20,10 @@ module JekyllNotion
 
     def read_notion_database
       databases.each do |db_config|
-        @current_db = NotionDatabase.new(:config => db_config)
-        @current_db.pages.each do |page|
-          @current_page = page
-          next if file_exists?(make_path)
-
-          current_collection.docs << make_page
-          log_new_page
-        end
-        # Store current collection
-        collections[current_db.collection] = current_collection
+        db = NotionDatabase.new(:config => db_config)
+        new_collection = CollectionGenerator.new(:db => db, :site => @site).generate
+        # Caching current collection
+        collections[db.collection] = new_collection
       end
     end
 
@@ -39,42 +33,6 @@ module JekyllNotion
 
     def collections
       @collections ||= {}
-    end
-
-    # Checks if a file already exists in the site source
-    def file_exists?(file_path)
-      File.exist? @site.in_source_dir(file_path)
-    end
-
-    def make_page
-      new_post = DocumentWithoutAFile.new(
-        make_path,
-        { :site => @site, :collection => current_collection }
-      )
-      new_post.content = make_md
-      new_post.read
-      new_post
-    end
-
-    def make_path
-      "_#{current_db.collection}/#{make_filename}"
-    end
-
-    def make_filename
-      if current_db.collection == "posts"
-        "#{current_page.created_time.to_date}-#{Jekyll::Utils.slugify(current_page.title,
-                                                                      :mode => "latin")}.md"
-      else
-        "#{current_page.title.downcase.parameterize}.md"
-      end
-    end
-
-    def make_md
-      NotionToMd::Converter.new(:page_id => current_page.id).convert(:frontmatter => true)
-    end
-
-    def current_collection
-      @site.collections[current_db.collection]
     end
 
     def config
@@ -99,17 +57,6 @@ module JekyllNotion
         return false
       end
       true
-    end
-
-    def log_new_page
-      Jekyll.logger.info("Jekyll Notion:", "Page => #{current_page.title}")
-      if @site.config.dig(
-        "collections", current_db.collection, "output"
-      )
-        Jekyll.logger.info("",
-                           "Path => #{current_collection.docs.last.path}")
-      end
-      Jekyll.logger.debug("", "Props => #{current_collection.docs.last.data.keys.inspect}")
     end
   end
 end
