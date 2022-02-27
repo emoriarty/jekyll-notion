6e8957018f768dc80f0b6d5d7f7e6ba124429b23
Enrique Arias <enrique.arias.cervero@gmail.com>
2022-02-27 12:01:41 +0100

    (chore) Log data pages (#37)

--- lib/jekyll-notion/data_generator.rb
+++ lib/jekyll-notion/data_generator.rb
@@ -6,12 +6,20 @@ module JekyllNotion
       @site.data[@db.data] = data
       # Caching current data
       @plugin.data[@db.data] = data
+      log_pages
     end
 
     private
 
     def data
       @data ||= @db.pages.map(&:props)
+    end
+
+    def log_pages
+      @db.pages.each do |page|
+        Jekyll.logger.info("Jekyll Notion:", "Page => #{page.title}")
+        Jekyll.logger.debug("", "Props => #{page.props.keys.inspect}")
+      end
     end
   end
 end
