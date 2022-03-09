95fab4a88ec5838fb407d79856e7390dfeff7b76
Enrique Arias <emoriarty81@gmail.com>
2022-03-09 11:25:04 +0100

    (fix) move collection_name and data_name to right class

--- lib/jekyll-notion/abstract_generator.rb
+++ lib/jekyll-notion/abstract_generator.rb
@@ -11,13 +11,5 @@ module JekyllNotion
     def generate
       raise "Do not use the AbstractGenerator class. Implement the generate method in a subclass."
     end
-
-    def collection_name
-      raise "Do not use the AbstractGenerator class. Implement the collection_name method in a subclass."
-    end
-
-    def data_name
-      raise "Do not use the AbstractGenerator class. Implement the data_name method in a subclass."
-    end
   end
 end
