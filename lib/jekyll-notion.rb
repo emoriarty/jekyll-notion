3ee2da0fc67e38f21befb5152fad2fed8850c770
Enrique Arias <emoriarty81@gmail.com>
2022-03-16 21:31:14 +0100

    (chore) organize folders

--- lib/jekyll-notion.rb
+++ lib/jekyll-notion.rb
@@ -13,15 +13,15 @@ Notion.configure do |config|
 end
 
 module JekyllNotion
-  autoload :DatabaseFactory, "jekyll-notion/database_factory"
-  autoload :PageFactory, "jekyll-notion/page_factory"
-  autoload :AbstractGenerator, "jekyll-notion/abstract_generator"
-  autoload :AbstractNotionResource, "jekyll-notion/abstract_notion_resource"
-  autoload :CollectionGenerator, "jekyll-notion/collection_generator"
-  autoload :DataGenerator, "jekyll-notion/data_generator"
-  autoload :PageGenerator, "jekyll-notion/page_generator"
+  autoload :DatabaseFactory, "jekyll-notion/factories/database_factory"
+  autoload :PageFactory, "jekyll-notion/factories/page_factory"
+  autoload :AbstractGenerator, "jekyll-notion/generators/abstract_generator"
+  autoload :DataGenerator, "jekyll-notion/generators/data_generator"
+  autoload :PageGenerator, "jekyll-notion/generators/page_generator"
+  autoload :CollectionGenerator, "jekyll-notion/generators/collection_generator"
   autoload :DocumentWithoutAFile, "jekyll-notion/document_without_a_file"
   autoload :PageWithoutAFile, "jekyll-notion/page_without_a_file"
+  autoload :AbstractNotionResource, "jekyll-notion/abstract_notion_resource"
   autoload :NotionDatabase, "jekyll-notion/notion_database"
   autoload :NotionPage, "jekyll-notion/notion_page"
 end
