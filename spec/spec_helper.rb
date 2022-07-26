097353aa41973bace6a37870ec0d836d1245eaf0
Enrique Arias <enrique.arias.cervero@gmail.com>
2022-07-26 15:45:22 +0200

    (chore) ruby 3.1 support (#58)
    * yaml load_file mandatory permitted_classes
    
    * ci test for ruby 3.1
    
    * support previous ruby versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,13 +13,51 @@ RSpec.configure do |config|
   SOURCE_DIR = File.expand_path("fixtures/my_site", __dir__)
   SOURCE_DIR_2 = File.expand_path("fixtures/my_site_2", __dir__)
   DEST_DIR = File.expand_path("dest", __dir__)
-  NOTION_RESULTS = YAML.load_file(File.expand_path("fixtures/notion/results.yml", __dir__))
-  NOTION_RESULTS_2 = YAML.load_file(File.expand_path("fixtures/notion/results_2.yml", __dir__))
-  NOTION_RESULTS_3 = YAML.load_file(File.expand_path("fixtures/notion/results_3.yml", __dir__))
-  NOTION_FILMS = YAML.load_file(File.expand_path("fixtures/notion/films.yml", __dir__))
-  NOTION_PAGE = YAML.load_file(File.expand_path("fixtures/notion/single_page.yml", __dir__))
-  NOTION_PAGE_BLOCKS = YAML.load_file(File.expand_path("fixtures/notion/single_page_blocks.yml",
-                                                       __dir__))
+  if RUBY_VERSION.start_with?("3.1")
+    NOTION_RESULTS = YAML.load_file(
+      File.expand_path("fixtures/notion/results.yml", __dir__),
+      :permitted_classes => [Hashie::Array, Notion::Messages::Message]
+    )
+    NOTION_RESULTS_2 = YAML.load_file(
+      File.expand_path("fixtures/notion/results_2.yml", __dir__),
+      :permitted_classes => [Hashie::Array, Notion::Messages::Message]
+    )
+    NOTION_RESULTS_3 = YAML.load_file(
+      File.expand_path("fixtures/notion/results_3.yml", __dir__),
+      :permitted_classes => [Hashie::Array, Notion::Messages::Message]
+    )
+    NOTION_FILMS = YAML.load_file(
+      File.expand_path("fixtures/notion/films.yml", __dir__),
+      :permitted_classes => [Hashie::Array, Notion::Messages::Message]
+    )
+    NOTION_PAGE = YAML.load_file(
+      File.expand_path("fixtures/notion/single_page.yml", __dir__),
+      :permitted_classes => [Hashie::Array, Notion::Messages::Message]
+    )
+    NOTION_PAGE_BLOCKS = YAML.load_file(
+      File.expand_path("fixtures/notion/single_page_blocks.yml", __dir__),
+      :permitted_classes => [Hashie::Array, Notion::Messages::Message]
+    )
+  else
+    NOTION_RESULTS = YAML.load_file(
+      File.expand_path("fixtures/notion/results.yml", __dir__),
+    )
+    NOTION_RESULTS_2 = YAML.load_file(
+      File.expand_path("fixtures/notion/results_2.yml", __dir__),
+    )
+    NOTION_RESULTS_3 = YAML.load_file(
+      File.expand_path("fixtures/notion/results_3.yml", __dir__),
+    )
+    NOTION_FILMS = YAML.load_file(
+      File.expand_path("fixtures/notion/films.yml", __dir__),
+    )
+    NOTION_PAGE = YAML.load_file(
+      File.expand_path("fixtures/notion/single_page.yml", __dir__),
+    )
+    NOTION_PAGE_BLOCKS = YAML.load_file(
+      File.expand_path("fixtures/notion/single_page_blocks.yml", __dir__),
+    )
+  end
   MD_FILES = Dir[File.expand_path("fixtures/md_files/*.md",
                                   __dir__)].each_with_object({}) do |file, memo|
     value = File.read(file)
