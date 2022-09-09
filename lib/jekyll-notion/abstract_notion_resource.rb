8e8c7198b1e479309af6cc2aeb8466f9aa814dab
Enrique Arias <enrique.arias.cervero@gmail.com>
2022-09-09 23:41:22 +0200

    (chore) update notion to md (#60)
    * adapt for notion_to_md
    
    * 2.3.0
    
    * update Gemfile.lock

--- lib/jekyll-notion/abstract_notion_resource.rb
+++ lib/jekyll-notion/abstract_notion_resource.rb
@@ -37,5 +37,11 @@ module JekyllNotion
       end
       true
     end
+
+    def build_blocks
+      NotionToMd::Blocks.build(block_id: id) do |nested_id|
+        @notion.block_children({ :block_id => nested_id })
+      end
+    end
   end
 end
