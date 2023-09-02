770fcc8ea23d68bda9ec144d565b516d9c2da1f1
Enrique Arias <emoriarty81@gmail.com>
2023-09-02 15:50:18 +0200

    clean logs

--- lib/jekyll-notion/cacheable.rb
+++ lib/jekyll-notion/cacheable.rb
@@ -2,23 +2,16 @@
 #
 
 module JekyllNotion
-  # Using VCR to record and playback Notion API responses for caching
   module Cacheable
     def database_query(*args)
-      puts "==> VCR DB item fetching #{args} from Notion..."
-
       VCR.use_cassette("#{args[0][:database_id]}") { super(*args) }
     end
 
     def block_children(*args)
-      puts "==> VCR BLOCK CHILDREN fetching #{args} from Notion..."
-
       VCR.use_cassette("#{args[0][:block_id]}") { super(*args) }
     end
 
     def page(*args)
-      puts "==> VCR PAGE fetching #{args} from Notion..."
-
       VCR.use_cassette("#{args[0][:page_id]}") { super(*args) }
     end
   end
