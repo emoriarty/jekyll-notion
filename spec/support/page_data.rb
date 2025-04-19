ad507280802fca9363b8896f8526fbf52dfbe557
Enrique Arias <emoriarty81@gmail.com>
2025-04-19 17:15:23 +0200

    wip

--- spec/support/page_data.rb
+++ spec/support/page_data.rb
@@ -29,7 +29,7 @@ RSpec.shared_examples "a jekyll data object" do |data_name|
   end
 
   it "stores select into the data object" do
-    expect(site.data[data_name]).to include("select" => "select1".dump)
+    expect(site.data[data_name]).to include("select" => "select1")
   end
 
   it "stores people into the data object" do
@@ -57,7 +57,7 @@ RSpec.shared_examples "a jekyll data object" do |data_name|
   end
 
   it "stores title into the data object" do
-    expect(site.data[data_name]).to include("title" => "Page 1".dump)
+    expect(site.data[data_name]).to include("title" => "Page 1")
   end
 
   it "stores date into the data object" do
